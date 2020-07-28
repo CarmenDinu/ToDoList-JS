97ae736c59d009f8e0ac9e71bcdc61a6cad6be89
Carmen <carmendinu5@yahoo.com>
2020-07-28 21:46:59 +0300

    update

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function addTasksFunction() {
 //Delete the tasks
 function deleteTasksFunction() {
   let table = document.getElementById("table");
-  let tableLength = table.rows.length - 1;
+  let tableLength = table.rows.length;
   let textDelete = document.getElementById("textDelete").value;
   if (textDelete > tableLength) {
     alert("Error! The number of tasks is below " + textDelete);
@@ -37,3 +37,27 @@ mouseOver.addEventListener("mouseenter", function (event) {
 mouseOver.addEventListener("mouseleave", function (event) {
   event.target.style.fontWeight = "normal";
 });
+
+//Filter
+
+let table = document.getElementById("table");
+let enterKey = document.getElementById("taskSearch");
+
+document
+  .getElementById("taskSearch")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      let searchFor = document.getElementById("taskSearch").value;
+      let nrOfRows = table.rows.length;
+      console.log("Press the enter key");
+      console.dir(table);
+      for (i = 0; i < nrOfRows; i++) {
+        let currentTaskName = table.rows[i].innerText;
+        if (currentTaskName.includes(searchFor)) {
+          table.rows[i].style.display = "block";
+        } else {
+          table.rows[i].style.display = "none";
+        }
+      }
+    }
+  });
